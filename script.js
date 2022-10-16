154895b244b7ffe0dc3cc5b729817223d31e5de6
danazou <dana.sou@gmail.com>
2022-10-15 23:14:07 -0700

    using trivia api to generate questions and loop through levels

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ const scienceNature = 10
 const levels = ['easy', 'medium', 'hard']
 
 
+function addGenre() {
 
+    const column = document.createElement('div')
+    column.classList.add('genre-column')
+    column.innerHTML = 'this is a genre'
+    game.append(column)
+
+    //use api for ease or factor out questions manually for practice? both?hm
+    //use below api to loop through levels
+    levels.forEach(level => {
+            fetch(`https://opentdb.com/api.php?amount=10&category=18&difficulty={level}&type=multiple`)
+
+    })
+
+}
+
+addGenre()
