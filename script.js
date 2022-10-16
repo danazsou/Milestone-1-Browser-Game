e60f98344a88afa0e6cc9534a7574b7214e4fbfb
danazou <dana.sou@gmail.com>
2022-10-16 12:25:41 -0700

    create first column

--- script.js
+++ script.js
@@ -17,12 +17,18 @@ function newCategory() {
     //use api for ease or factor out questions manually for practice? both?hm
     //use below api to loop through levels array using .forEach 
     levels.forEach(level => {
+        
+        //locally scoped below so okay to use const
+       const card = document.createElement('div')
+       card.classList.add('card')
+
 
         //will cosole log the data to see if api loops correctly
             fetch(`https://opentdb.com/api.php?amount=1&category=17&difficulty=${level}&type=multiple`)
             .then(response => response.json())
             .then(data => console.log(data))
     })
+
 
 }
 
